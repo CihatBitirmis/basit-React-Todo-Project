bfe92e42818a585ab76a040cc3906c745dbc1cd7
CihatBitirmiş <cihatbitirmis@gmail.com>
2022-12-29 14:23:53 +0300

    basitReactTodoProject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [add, setAdd] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (add === "") {
+      alert("Your To do List Empty!")
+      return
+    }
+    console.log(add)
+    setTodos([...todos,add])
+    setAdd("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <form onSubmit={handleSubmit}>
+        <h1 className="d-flex justify-content-center">Cihat Bitirmiş</h1>
+        <div className="input-group mt-5">
+          <input type="text"
+            className="form-control"
+            placeholder="Your To Do List"
+            value={add}
+            onChange={(event) => {
+              setAdd(event.target.value)
+            }} />
+          <button className="btn btn-outline-light bg-success w-25" type="submit">ADD</button>
+        </div>
+      </form>
+      <div className="mt-5">
+        {
+          todos.length === 0 ? (
+            <p>You Don't have any todos yet</p>
+          ) : (
+            <>
+              {
+                todos.map((item, index) => (
+                  <h1 key={index}>{item}</h1>
+                ))
+              }
+            </>
+          )
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
